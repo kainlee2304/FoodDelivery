c96ffa0d0f92bc1ecee49360445a426d969996b0
vy34 <nguyenthingocvy332004@gmail.com>
2024-04-09 22:51:42 +0700

    Notification and profile

--- app/src/main/java/com/example/wavesoffood/MainActivity.kt
+++ app/src/main/java/com/example/wavesoffood/MainActivity.kt
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.wavesoffood.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         var NavController = findNavController(R.id.fragmentContainerView)
         var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomnav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener{
+            val bottomSheetDialog=NotificationBottom_Fragment()
+            bottomSheetDialog.show(supportFragmentManager,"Test")
+        }
     }
 }
